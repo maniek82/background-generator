ac243ec4d0e0eb1424e08f0fdea1db4297b4b2d0
maniek82 <mariusz.tripler@gmail.com>
2018-09-01 23:14:44 +0100

    Added copy css color code from input functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var cssInputColor = document.querySelector(".cssLine");
+var cssInputColor = document.querySelector(".colorInput");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.getElementById("gradient");
+var copyButton = document.querySelector(".copyButton");
 
 
 function setInputInitialValue() {
@@ -13,9 +14,24 @@ function setGradient() {
 	cssInputColor.value = body.style.background+";";
 }
 
+function copyColorFromInput() {
+	var color = "background: linear-gradient(to right, " + color1.value + ", " + color2.value + ");"
+	cssInputColor.select();
+	document.execCommand("copy");
+	console.log("Your color: " + color);
+	alert("Your picked color is: "+ color);
+	copyButton.textContent = "Copied";
+  
+	setTimeout(() => {
+	  cssInputColor.blur();
+	  copyButton.textContent = "Copy Code";
+	}, 2000);
+  };
+  
 setInputInitialValue();
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
+copyButton.addEventListener("click",copyColorFromInput);
 	
 
