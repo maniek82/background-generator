ffb51f98285bce7278c5f4023da074633097bc98
maniek82 <mariusz.tripler@gmail.com>
2018-09-01 23:00:44 +0100

    Added initial value to input

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var css = document.querySelector(".cssLine");
+var cssInputColor = document.querySelector(".cssLine");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.getElementById("gradient");
 
 
+function setInputInitialValue() {
+	cssInputColor.value = "background: linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
+  };
+
 function setGradient() {
 	body.style.background = "linear-gradient(to right,"+color1.value +", "+color2.value+ ")";
-	css.value = body.style.background+";";
+	cssInputColor.value = body.style.background+";";
 }
+
+setInputInitialValue();
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
